37be531e6ecbdde957c52e62bf3b8d8a7a311694
CTower87 <christian@normanfamily.org>
2018-07-06 01:37:29 +0000

    multicolered moving snake

--- unicornhat/moving_snake.py
+++ unicornhat/moving_snake.py
@@ -13,8 +13,11 @@ try:
 
             points = [(0,0), (1,0), (2,1), (3,1), (4,2), (5,3), (5,4), (6,5), (5,6), (4,7), (3,7), (2,7), (1,6), (0,5), (0,4), (1,3), (1,2), (0,1)]
 
-            uc.set_pixel(points[x][0],points[x][1],128,0,0)
-            uc.set_pixel(points[x-4][0],points[x-4][1],0,0,0)
+            for y in range (0,5):
+                colors = [(128,0,0), (255,51,204), (51,51,255,), (0,255,255), (0,0,0)]
+            
+                uc.set_pixel(points[x-y][0],points[x-y][1],colors[y][0],colors[y][1],colors[y][2])
+                
             uc.show()
             time.sleep(.3)
     
